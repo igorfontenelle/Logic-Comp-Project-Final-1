143926a9f0478fa6ae7c61187bcc04eb03dfd2f2
igorfontenelle <igorfelias09@gmail.com>
2023-12-05 23:34:38 -0300

    ###Removed   - prints desnecessários
    ###Added
      - Imagem para auxiliar onde alterar a quantidade de cursos e de slots disponíveis.

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Logic-Comp-Project-Final-1
-Projeto Final 1 da discipla de Lógica para computação
-Tema: Definição dos Horários de Cursos em um Evento.
+
+* Projeto Final 1 da discipla de Lógica para computação
+* Tema: Definição dos Horários de Cursos em um Evento.
+* Equipe: Igor Fontenele Elias
+
+
--- main.py
+++ main.py
@@ -1,17 +1,12 @@
-from z3 import * #import the z3 solver
-
 from z3 import *
 
 def resolver_agendamento_cursos(k, m, inscricoes_em_comum):
     # Criar as variáveis booleanas para representar os minicursos nos slots
     slots = [[Bool(f"x_{i}_{j}") for j in range(m)] for i in range(1, k+1)]
-    print(slots)
     # Restrição: Cada minicurso deve ser ofertado em pelo menos um slot
     restricao_1 = [Or(slots[i-1]) for i in range(1, k+1)]
-    print(restricao_1)
     # Restrição: Cada minicurso deve ser ofertado em no máximo um slot
     restricao_2 = [Implies(slots[i-1][j], And([Not(slots[i-1][k]) for k in range(m) if k != j])) for i in range(1, k+1) for j in range(m)]
-    print(restricao_2)
     # Restrição: Minicursos com inscrições em comum não podem ser ofertados no mesmo slot
     restricao_3 = [Or(Not(slots[p-1][j]), Not(slots[q-1][j])) for (p, q) in inscricoes_em_comum for j in range(m)]
 
@@ -32,6 +27,7 @@ def resolver_agendamento_cursos(k, m, inscricoes_em_comum):
     else:
         return None
 
+# Defina aqui as entradas, sendo a variável "k" a quantidade de cursos e a variável "m" o número de slots
 # Exemplo de entrada 1
 k = 4
 m = 3
@@ -48,12 +44,14 @@ horarios = resolver_agendamento_cursos(k, m, inscricoes_em_comum)
 horarios1 = resolver_agendamento_cursos(k1, m1, inscricoes_em_comum1)
 
 # Exibir a saída
+print('Saída 1: ')
 if horarios:
     for curso, horario in horarios.items():
         print(f"{curso} s{horario}")
 else:
     print("Não é possível agendar os cursos respeitando as inscrições em comum.")
 print('----------')
+print('Saída 2: ')
 if horarios1:
     for curso, horario in horarios1.items():
         print(f"{curso} s{horario}")
